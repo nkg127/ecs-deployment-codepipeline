9270b65510582fef878b98a373609a091f840fa2
nkg127 <88700619+nkg127@users.noreply.github.com>
2022-07-18 17:04:50 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -18,6 +18,6 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 
 @app.get("/", tags=['Root Endpoint'])
 async def root():
-    return {"message": "Welcome to FastAPI Email Service!"}
+    return {"message": "Welcome to FastAPI Email Service - update1!"}
 
 app.include_router(api_router, prefix="/api")
